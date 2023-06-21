fd301dffa5da87d522ce1a0e30cf8014362813c5
engNesta <124577220+engNesta@users.noreply.github.com>
2023-06-21 21:32:48 +0200

    Created test case to fully implement

--- Tests/EditorTest.cpp
+++ Tests/EditorTest.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Nesta on 2023-06-20.
 //
+#define CATCH_CONFIG_MAIN
+#include "../Source/MainComponent.h"
+#include <juce_core/juce_core.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include "Catch2/src/catch2/catch_test_macros.hpp"
+
+TEST_CASE("Plugin Editor GUI Display Test", "[plugin_editor]")
+{
+    juce::AudioPluginFormatManager formatManager;
+    // ... Initialize the format manager and load plugins if needed
+    formatManager.addDefaultFormats();
+
+    juce::String errorMessage;
+
+    std::unique_ptr<juce::AudioPluginInstance> pluginInstance = formatManager.createPluginInstance(/* Plugin parameters */);
+    REQUIRE(pluginInstance != nullptr);
+
+    juce::AudioProcessorEditor* editor = pluginInstance->createEditor();
+    REQUIRE(editor != nullptr);
+
+    // Add assertions to check if the editor is properly displayed
+    REQUIRE(editor->getWidth() == 300);
+    REQUIRE(editor->getHeight() == 200);
+
+
+}
